ca9b439c4b061a56c0cb85d79f4e97c5b94bb91a
dharani0110 <1928026@kiit.ac.in>
2023-11-09 20:54:23 -0800

    Update Categories.js
    refactoring

--- Categories.js
+++ Categories.js
@@ -1,17 +1,19 @@
-// const value = localStorage.getItem('nameInput');
-// value.innerHTML=value;
+
+function getPlayerNameFromLocalStorage() {
+    return localStorage.getItem("playerName");
+}
 
 document.addEventListener("DOMContentLoaded", function() {
-    // Access the player name from localStorage
-    const playerName = localStorage.getItem("playerName");
+    const playerName = getPlayerNameFromLocalStorage();
 
     if (playerName) {
         console.log("Player's name retrieved from localStorage: " + playerName);
-       
     } else {
         console.log("Player's name not found in localStorage.");
     }
-
-
-    
 });
+
+// Export the function for testing
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = { getPlayerNameFromLocalStorage };
+}
